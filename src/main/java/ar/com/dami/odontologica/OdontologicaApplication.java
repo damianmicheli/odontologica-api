1f25f66e467f3e73dc854fb215de2898e19b908a
Damian Micheli <damianmicheli@gmail.com>
2022-07-09 15:25:55 -0300

    Ajustes dev - production

--- src/main/java/ar/com/dami/odontologica/OdontologicaApplication.java
+++ src/main/java/ar/com/dami/odontologica/OdontologicaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @RestController
