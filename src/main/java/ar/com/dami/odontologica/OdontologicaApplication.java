343de0a0da42228d25c36973c60fb634cc74a6e5
Damian Micheli <damianmicheli@gmail.com>
2022-07-09 12:07:31 -0300

    Configuracion CORS

--- src/main/java/ar/com/dami/odontologica/OdontologicaApplication.java
+++ src/main/java/ar/com/dami/odontologica/OdontologicaApplication.java
@@ -3,6 +3,8 @@ package ar.com.dami.odontologica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OdontologicaApplication {
@@ -10,5 +12,14 @@ public class OdontologicaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OdontologicaApplication.class, args);
 	}
-
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://odontologica.dami.com.ar");
+			}
+		};
+	}
 }
+
+
